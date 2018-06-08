5008cdba7aba684ac969f3f91dd805874ff2428d
csdoker <csd758371536@qq.com>
2018-06-08 13:47:46 +0800

    modify

---
+++ vue-travel/src/pages/detail/Detail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
+export default {
+  name: 'Detail',
+  components: {
+    DetailBanner,
+    DetailHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .content
+    height: 50rem
+</style>
---
+++ vue-travel/src/pages/detail/components/Header.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <router-link v-show="showAbs" tag="div" :to="{ name: 'Home', params: {} }" class="header-abs">
+      <div class="iconfont header-abs-back">&#xe624;</div>
+    </router-link>
+    <div :style="opacityStyle" v-show="!showAbs" class="header-fixed">
+      <router-link :to="{ name: 'Home', params: {} }">
+        <div class="iconfont header-fixed-back">&#xe624;</div>
+      </router-link>
+      景点详情
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      if (top > 60) {
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = { opacity }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  // 页面展示时绑定事件
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  // 页面离开时解绑事件
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header-abs
+    position: absolute
+    left: .2rem
+    top: .2rem
+    width: .8rem
+    height: .8rem
+    border-radius: .4rem
+    text-align: center
+    line-height: .8rem
+    background-color: rgba(0, 0, 0, .8)
+    .header-abs-back
+      color: #fff
+      font-size: .4rem
+  .header-fixed
+    position: fixed
+    top: 0
+    left: 0
+    right: 0
+    height: $headerHeight
+    line-height: $headerHeight
+    text-align: center
+    color: #fff
+    background: $bgColor
+    font-size: .32rem
+    .header-fixed-back
+      position: absolute
+      top: 0
+      left: 0
+      width: .64rem
+      text-align: center
+      font-size: .4rem
+      color: #fff
+</style>

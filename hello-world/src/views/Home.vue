42acb3a0c23caee75ccfa8923f33be173628e596
csdoker <csd758371536@qq.com>
2018-10-18 18:05:00 +0800

    add sell project

--- hello-world/src/views/Home.vue
+++ hello-world/src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="mountNode"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import G6 from '@antv/g6'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      graphData: {
+        nodes: [{
+          id: 'node1',
+          x: 100,
+          y: 200
+        }, {
+          id: 'node2',
+          x: 300,
+          y: 200
+        }],
+        edges: [{
+          id: 'edge1',
+          target: 'node2',
+          source: 'node1'
+        }]
+      }
+    }
+  },
+  mounted () {
+    const layout = new G6.Layouts.CompactBoxTree({
+      direction: 'LR'
+    })
+    const data = {
+      roots: [{
+        label: 'root',
+        children: [{
+          label: 'child1',
+          children: [
+            {
+              label: 'child\n1.1'
+            }
+          ]
+        }, {
+          label: 'child2'
+        }]
+      }]
+    }
+    const tree = new G6.Tree({
+      container: 'mountNode',
+      width: 500,
+      height: 500,
+      layout,
+      fitView: 'cc'
+    })
+    tree.read(data)
   }
 }
 </script>
